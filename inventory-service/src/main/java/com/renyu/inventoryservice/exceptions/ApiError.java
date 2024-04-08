15ca941a71f7a2cc6bb81e6a0cd73860b85081fb
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-04-08 15:14:29 -0300

    Adicionado Custom Exception Handler

---
+++ inventory-service/src/main/java/com/renyu/inventoryservice/exceptions/ApiError.java
@@ -0,0 +1,29 @@
+package com.renyu.inventoryservice.exceptions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class ApiError {
+    private String resume;
+    private HttpStatus status;
+    private List<String> errors;
+
+    public ApiError(final HttpStatus status, final String resume, final String error) {
+        this.status = status;
+        this.resume = resume;
+        this.errors = Collections.singletonList(error);
+    }
+
+    public ApiError(final HttpStatus status, final String resume, final List<String> errors) {
+        this.status = status;
+        this.resume = resume;
+        this.errors = errors;
+    }
+
+}
