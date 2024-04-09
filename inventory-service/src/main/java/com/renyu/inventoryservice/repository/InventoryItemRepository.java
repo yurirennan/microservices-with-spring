7da2da3b58c95acfd74029913be653911620648d
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-04-09 16:33:52 -0300

    Criação de método para retornar os produtos em estoque filtrados pelo sku_code

--- inventory-service/src/main/java/com/renyu/inventoryservice/repository/InventoryItemRepository.java
+++ inventory-service/src/main/java/com/renyu/inventoryservice/repository/InventoryItemRepository.java
@@ -2,8 +2,13 @@ package com.renyu.inventoryservice.repository;
 
 import com.renyu.inventoryservice.model.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
+    @Query("SELECT item FROM InventoryItem item WHERE item.skuCode in (?1) and item.quantity > 0")
+    List<InventoryItem> findBySkuCodeAndQuantityGreaterThanZero(List<String> skuCodes);
 }
